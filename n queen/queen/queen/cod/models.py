9c1b1eedba39287b02148673579248c91756aecc
SURENDHAN <142890429+SURENDHAN@users.noreply.github.com>
2024-07-30 13:05:15 +0530

    Delete n queen/queen/queen directory

--- n queen/queen/queen/cod/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-class number(models.Model):
-    qe=models.IntegerField()
--- n queen/queen/queen/cod/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.nqueens_view, name='nqueens_view'),
-]
